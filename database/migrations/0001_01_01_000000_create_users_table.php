93de35e0ac9c753ba0fdd778f3693b3e9d97b44c
IkbalMift <ikbalmiftahudin1@gmail.com>
2026-04-21 13:32:14 +0700

    perbaikan migrations user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,1 +1,53 @@
-c
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name','email','password','is_admin','profile_photo'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
+    public function reseps() {
+        return $this->hasMany(Resep::class);
+    }
+
+    public function favorites() {
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function feedbacks() {
+        return $this->hasMany(Feedback::class);
+    }
+
+    public function fridge() {
+        return $this->hasMany(UserFridge::class);
+    }
+
+    public function mealPlanners() {
+        return $this->hasMany(MealPlanner::class);
+    }
+
+    public function following() {
+        return $this->hasMany(UserFollow::class, 'user_id');
+    }
+
+    public function followers() {
+        return $this->hasMany(UserFollow::class, 'to_user_id');
+    }
+}
