f756a86b76c80177b3ff1e5554eac6e9b6083bbf
Harmoni Natanael S <harmoninatanaels@gmail.com>
2026-04-28 15:55:12 +0700

    fixing migration problem

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,53 +1,50 @@
 <?php
 
-namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
+return new class extends Migration
 {
-    use HasFactory, Notifiable;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
 
-    protected $fillable = [
-        'name','email','password','is_admin','profile_photo'
-    ];
+            $table->timestamps();
+        });
 
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
 
-    protected $casts = [
-        'password' => 'hashed',
-    ];
-
-    public function reseps() {
-        return $this->hasMany(Resep::class);
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
-    public function favorites() {
-        return $this->hasMany(Favorite::class);
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
-
-    public function feedbacks() {
-        return $this->hasMany(Feedback::class);
-    }
-
-    public function fridge() {
-        return $this->hasMany(UserFridge::class);
-    }
-
-    public function mealPlanners() {
-        return $this->hasMany(MealPlanner::class);
-    }
-
-    public function following() {
-        return $this->hasMany(UserFollow::class, 'user_id');
-    }
-
-    public function followers() {
-        return $this->hasMany(UserFollow::class, 'to_user_id');
-    }
-}
+};
