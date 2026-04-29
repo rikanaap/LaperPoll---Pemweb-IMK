0bb63a0d21afe16ac0fbc48de8ee8e70e8c6da67
IkbalMift <ikbalmiftahudin1@gmail.com>
2026-04-30 05:54:20 +0700

    fix : Perbaikan code migrations user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
 
+            $table->string('password');
+
+            $table->boolean('is_admin')->default(false);
+            $table->string('profile_photo')->nullable();
+
+            $table->rememberToken();
             $table->timestamps();
         });
 
@@ -30,9 +35,15 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
+
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
@@ -43,8 +54,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('users');
     }
 };
