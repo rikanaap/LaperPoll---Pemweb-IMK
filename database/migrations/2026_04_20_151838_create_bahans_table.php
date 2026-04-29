3a792e9d5dbb6ceb35d00c34b3a2a73c37cbd009
IkbalMift <ikbalmiftahudin1@gmail.com>
2026-04-29 19:22:56 +0700

    test

--- database/migrations/2026_04_20_151838_create_bahans_table.php
+++ database/migrations/2026_04_20_151838_create_bahans_table.php
@@ -4,11 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migration // Jangan diubah baris ini
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('bahans', function (Blueprint $table) {
@@ -18,9 +15,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('bahans');
