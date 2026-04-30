a5b66aa1806ed3b24f73f46222b350da9f0ed107
BallRmd <iqbalrmd1206@gmail.com>
2026-04-30 23:54:27 +0700

    migrasi detail dan timer resep

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('user.landing');
 });
+
+Route::get('/detail-resep', function () {
+    return view('pages.detail_resep.detail_resep');
+});
