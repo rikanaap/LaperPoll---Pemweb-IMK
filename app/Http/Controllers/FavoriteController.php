c8b55204bdc29b81cd735e89ff52408f1da29bf1
IkbalMift <ikbalmiftahudin1@gmail.com>
2026-06-07 21:34:11 +0700

    feat(controller) = menambahkan ->where('is_published', true) setiap yang akses resep

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -12,7 +12,7 @@ class FavoriteController extends Controller
     public function toggle($id)
     {
         $user = Auth::user();
-        Resep::findOrFail($id); // pastikan resep exist
+        Resep::where('is_published', true)->findOrFail($id);
 
         // toggle() return array ['attached'=>[], 'detached'=>[]]
         $result     = $user->favorites()->toggle($id);
@@ -31,6 +31,7 @@ class FavoriteController extends Controller
             ->favorites()
             ->with('user') // eager load author
             ->withPivot('created_at')
+            ->where('is_published', true)
             ->orderBy('favorites.created_at', 'desc')
             ->get();
 
